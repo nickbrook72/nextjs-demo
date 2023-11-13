b8bf712bde8f4b0e0143021ebfd7a941266b0f16
Nick Brook <nickbrook72@gmail.com>
2023-11-13 18:11:40 +0000

    Fix the test

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,7 +1,7 @@
 describe('list search page', () => {
   it('should allow searching', () => {
     cy.visit('/');
-    cy.get('h1').should('contain.text', 'Really simpleXXX');
+    cy.get('h1').should('contain.text', 'Really simple');
     cy.findByTestId('next-page-button').click();
     cy.get('h2').should('contain.text', 'next page');
     cy.findByTestId('search-box').type('Hello');
